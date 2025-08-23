6ae6ed50547c91c1e453b2e9f104b8ead7f46de6
azeuusa <reza.fahrezawe@gmail.com>
2025-08-23 13:30:38 +0700

    Add files via upload

---
+++ proyek.py
@@ -0,0 +1,18 @@
+print("==================================")
+print("      wellcome to calculator goblok")
+print("==================================")
+print("pilih ya kontol anjing babi / * - +")
+q = input("pilih ya yang di atas itu goblok;:::::")
+a = int(input("masukan angka"))
+b = int(input("lagi goblok"))
+
+if q == "/":
+   print("hasilnya guoblok:", a / b)
+elif q == "+":
+   print("goblok nih", a + b)
+elif q == "*":
+   print("ini ya goblokkkk lihat nih, a * b")
+elif q == "-":
+   print ("anjing nih ya goblog", a - b)
+else:
+  print("nih ya tolol") 
